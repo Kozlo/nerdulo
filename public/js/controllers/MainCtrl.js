dfc555926b2475d8e240ad73552c1e4ae23d70c6
Martins Kozlovskis <mkozlovskis@gmail.com>
2015-03-24 10:33:46 +0200

    Fixed a typo

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,5 @@
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-    $scope.tagline = 'Perpare to be mindblown!';
+    $scope.tagline = 'Perpare to be mind `blown!';
 
 });
